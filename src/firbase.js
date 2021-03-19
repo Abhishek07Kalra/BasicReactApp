a8065e1ec5afad733efcace2a6d3338bd3b471c0
Abhishek kalra <abhishekkalra560@gmail.com>
2021-03-19 14:08:17 +0530

    first

--- src/firbase.js
+++ src/firbase.js
@@ -1,11 +1,6 @@
 import firebase from 'firebase';
 var firebaseConfig = {
-    apiKey: "AIzaSyBi3A5YhcFsIyfCG9iuOBprD1RYQZVFAuY",
-    authDomain: "gfgfirestore.firebaseapp.com",
-    projectId: "gfgfirestore",
-    storageBucket: "gfgfirestore.appspot.com",
-    messagingSenderId: "744866265074",
-    appId: "1:744866265074:web:bf51a045b6f9afaf4f1d6b"
+    // your credentials here.
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
